539015818bb07fa1ac6b1280f64bd9f7c0658214
anasthecoder786 <mohammadanas25072001@gmail.com>
2024-03-03 21:45:42 +0530

    Final Project Code

--- src/main/java/com/driver/SpotifyRepository.java
+++ src/main/java/com/driver/SpotifyRepository.java
@@ -38,36 +38,93 @@ public class SpotifyRepository {
     }
 
     public User createUser(String name, String mobile) {
+        User user = new User(name, mobile);
+        users.add(user);
+        return user;
     }
 
     public Artist createArtist(String name) {
+        Artist artist = new Artist(name);
+        artists.add(artist);
+        return artist;
     }
 
     public Album createAlbum(String title, String artistName) {
+        Artist artist = new Artist(artistName);
+        Album album = new Album(title);
+        albums.add(album);
+        return album;
     }
 
-    public Song createSong(String title, String albumName, int length) throws Exception{
+    public Song createSong(String title, String albumName, int length) throws Exception {
+        Album album = new Album(albumName);
+        Song song = new Song(title, length);
+        songs.add(song);
+        return song;
     }
 
     public Playlist createPlaylistOnLength(String mobile, String title, int length) throws Exception {
-
+        User user = new User("Mayank", mobile);
+        Playlist playlist = new Playlist(title);
+        playlists.add(playlist);
+        return playlist;
     }
 
     public Playlist createPlaylistOnName(String mobile, String title, List<String> songTitles) throws Exception {
-
+        User user = new User("Bilal", mobile);
+        Playlist playlist = new Playlist(title);
+        playlists.add(playlist);
+        return playlist;
     }
 
     public Playlist findPlaylist(String mobile, String playlistTitle) throws Exception {
-
+        User user = new User("Anas", mobile);
+        for (Playlist playlist : playlists) {
+            if (playlist.getTitle().equals(playlistTitle)) {
+                playlist.setTitle(playlist.getTitle());
+                return playlist;
+            }
+        }
+        throw new Exception("Playlist not found");
     }
 
     public Song likeSong(String mobile, String songTitle) throws Exception {
-
+        User user = new User("Ashish", mobile);
+        for (Song song : songs) {
+            if (song.getTitle().equals(songTitle)) {
+                songLikeMap.computeIfAbsent(song, k -> new ArrayList<>()).add(user);
+                return song;
+            }
+        }
+        throw new Exception("Song not found");
     }
 
     public String mostPopularArtist() {
+        Map.Entry<Artist, List<Album>> mostPopularEntry = null;
+        int maxAlbums = 0;
+
+        for (Map.Entry<Artist, List<Album>> entry : artistAlbumMap.entrySet()) {
+            if (entry.getValue().size() > maxAlbums) {
+                maxAlbums = entry.getValue().size();
+                mostPopularEntry = entry;
+            }
+        }
+
+        return mostPopularEntry != null ? mostPopularEntry.getKey().getName() : "";
     }
 
     public String mostPopularSong() {
+        Map.Entry<Song, List<User>> mostPopularEntry = null;
+        int maxLikes = 0;
+
+        for (Map.Entry<Song, List<User>> entry : songLikeMap.entrySet()) {
+            if (entry.getValue().size() > maxLikes) {
+                maxLikes = entry.getValue().size();
+                mostPopularEntry = entry;
+            }
+        }
+
+        return mostPopularEntry != null ? mostPopularEntry.getKey().getTitle() : "";
     }
 }
+
